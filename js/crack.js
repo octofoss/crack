2dc7e3671e7b9635755a76a8934b3b7b546587ad
octofoss <32109984+octofoss@users.noreply.github.com>
2023-09-16 15:05:14 -0400

    button

--- js/crack.js
+++ js/crack.js
@@ -7,3 +7,26 @@ setTimeout(() => {
         moth.textContent = "hey, your resolution's a bit low. consider switching to 1026px or wider"
     }
 }, 100)
+
+// code taken directly from c.ob literally barely even modified, all i changed was the element names. soz
+window.addEventListener('mousedown', ev=> {
+    if(ev.target.id == "cenu-icon") {
+        MUI("toggle")
+
+    //if clicking on something under the meta menu, do whatever the link is for
+    } else if(ev.target.parentElement.id == "cenu-links"){
+        if(!body.classList.contains('in-menu')) {
+            switch(ev.target.id) {
+
+                case "cenu-obs":
+                    toggleEntMenu()
+                break
+
+                case "cenu-sys":
+                    toggleSysMenu()
+                break
+            }
+        }
+    }
+}
+)
