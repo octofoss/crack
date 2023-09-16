197e75eac48d90049b6a98d6ef9ec7e43d492154
octofoss <32109984+octofoss@users.noreply.github.com>
2023-09-16 15:24:07 -0400

    styles more

--- js/crack.js
+++ js/crack.js
@@ -8,25 +8,9 @@ setTimeout(() => {
     }
 }, 100)
 
-// code taken directly from c.ob literally barely even modified, all i changed was the element names. soz
-window.addEventListener('mousedown', ev=> {
-    if(ev.target.id == "cenu-icon") {
-        MUI("toggle")
-
-    //if clicking on something under the meta menu, do whatever the link is for
-    } else if(ev.target.parentElement.id == "cenu-links"){
-        if(!body.classList.contains('in-menu')) {
-            switch(ev.target.id) {
-
-                case "cenu-obs":
-                    toggleEntMenu()
-                break
-
-                case "cenu-sys":
-                    toggleSysMenu()
-                break
-            }
-        }
+window.addEventListener('mousedown', ev => {
+    if (ev.target.id == "cenu-icon") {
+        // crack ui... NOT mindspike ui.. trust...
+        CUI("toggle")
     }
-}
-)
+})
