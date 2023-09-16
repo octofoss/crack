1c466ccd9c2584a0a1e497f6a1a5775b9b300f5f
octofoss <32109984+octofoss@users.noreply.github.com>
2023-09-16 14:10:49 -0400

    styles

--- js/crack.js
+++ js/crack.js
@@ -2,6 +2,8 @@ var content = document.querySelector('#content')
 var moth = document.querySelector('.mothmsg')
 
 // window TINY warning
-if (window.innerWidth < 1026) {
-    moth.textContent = "hey, your resolution's a bit low. consider switching to 1026px or wider"
-}
+setTimeout(() => {
+    if (window.innerWidth < 1026) {
+        moth.textContent = "hey, your resolution's a bit low. consider switching to 1026px or wider"
+    }
+}, 100)
